3d3565f92252420129fa7a743e898f92b3e8a121
Sunasuna24 <sunasunayaka1218@gmail.com>
2022-09-22 20:07:38 +0900

    CSSを読み込んだ

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,9 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>LaravelBnb</title>
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Nunito:200,600">
         <script src="{{ asset('js/app.js') }}" defer></script>
+        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     </head>
     <body>
         <div id="app">
