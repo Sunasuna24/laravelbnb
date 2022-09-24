78f27cf3f20b85499a8420b4a96df6fd6ad8c839
Sunasuna24 <sunasunayaka1218@gmail.com>
2022-09-25 00:20:35 +0900

    vueからapiを叩いた

--- resources/js/bookable/Availability.vue
+++ resources/js/bookable/Availability.vue
@@ -11,7 +11,7 @@
                 <input v-model="to" type="text" name="to" id="to" class="form-control form-control-sm" placeholder="End Date" @keyup.enter="check">
             </div>
         </div>
-        <button class="btn btn-secondary btn-block" @click="check">Check!</button>
+        <button class="btn btn-secondary btn-block" @click="check" :disabled="loading">Check!</button>
     </div>
 </template>
 <script>
@@ -19,12 +19,26 @@ export default {
     data() {
         return {
             from: null,
-            to: null
+            to: null,
+            loading: false,
+            status: null,
+            errors: null
         }
     },
     methods: {
         check() {
-            alert('I will check something now!');
+            this.loading = true;
+            this.errors = null;
+
+            axios.get(`/api/bookables/${this.$route.params.id}/availability?from=${this.from}&to=${this.to}`)
+                .then(response => {
+                    this.status = response.status;
+                }).catch(error => {
+                    if (error.response.status === 422) {
+                        this.errors = error.response.data.errors;
+                    }
+                    this.status = error.response.status;
+                }).then(() => this.loading = false);
         }
     }
 }
