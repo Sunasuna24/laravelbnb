c76d0f9a0ffe839d258fc4be454225e562b65efa
Sunasuna24 <sunasunayaka1218@gmail.com>
2022-09-24 20:57:18 +0900

    bootstrapを使って詳細画面をデザインした

--- public/js/app.js
+++ public/js/app.js
@@ -1911,14 +1911,16 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony default export */ __webpack_exports__["default"] = ({
   data: function data() {
     return {
-      bookable: null
+      bookable: null,
+      loading: true
     };
   },
   created: function created() {
     var _this = this;
 
     axios.get("/api/bookables/".concat(this.$route.params.id)).then(function (response) {
-      return _this.bookable = response.data;
+      _this.bookable = response.data;
+      _this.loading = false;
     });
   }
 });
@@ -2063,7 +2065,17 @@ var render = function render() {
   var _vm = this,
       _c = _vm._self._c;
 
-  return _c("div", [_vm._v("\n    xxx\n")]);
+  return _c("div", {
+    staticClass: "row"
+  }, [_c("div", {
+    staticClass: "col-md-8"
+  }, [_c("div", {
+    staticClass: "card"
+  }, [_c("div", {
+    staticClass: "card-body"
+  }, [!_vm.loading ? _c("div", [_c("h2", [_vm._v(_vm._s(_vm.bookable.title))]), _vm._v(" "), _c("hr"), _vm._v(" "), _c("article", [_vm._v(_vm._s(_vm.bookable.description))])]) : _c("div", [_vm._v("Loading...")])])])]), _vm._v(" "), _c("div", {
+    staticClass: "col-md-4"
+  }, [_vm._v("Here comes availability & prices.")])]);
 };
 
 var staticRenderFns = [];
--- resources/js/bookable/Bookable.vue
+++ resources/js/bookable/Bookable.vue
@@ -1,18 +1,33 @@
 <template>
-    <div>
-        xxx
+    <div class="row">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-body">
+                    <div v-if="!loading">
+                        <h2>{{ bookable.title }}</h2>
+                        <hr>
+                        <article>{{ bookable.description }}</article>
+                    </div>
+                    <div v-else>Loading...</div>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-4">Here comes availability & prices.</div>
     </div>
 </template>
 <script>
 export default {
     data() {
         return {
-            bookable: null
+            bookable: null,
+            loading: true
         }
     },
     created() {
-        axios.get(`/api/bookables/${this.$route.params.id}`)
-            .then(response => (this.bookable = response.data));
+        axios.get(`/api/bookables/${this.$route.params.id}`).then(response => {
+            this.bookable = response.data;
+            this.loading = false;
+        });
     }
 }
 </script>
