8778b0b72747de0218fa8b13913018a3e2051f69
Sunasuna24 <sunasunayaka1218@gmail.com>
2022-09-24 22:51:56 +0900

    フォームの値をvueで取得した

--- resources/js/bookable/Availability.vue
+++ resources/js/bookable/Availability.vue
@@ -4,16 +4,26 @@
         <div class="form-row">
             <div class="form-group col-md-6">
                 <label for="from">From</label>
-                <input type="text" name="from" id="from" class="form-control form-control-sm" placeholder="Start Date">
+                <input v-model="from" type="text" name="from" id="from" class="form-control form-control-sm" placeholder="Start Date">
             </div>
             <div class="form-group col-md-6">
                 <label for="to">To</label>
-                <input type="text" name="to" id="to" class="form-control form-control-sm" placeholder="End Date">
+                <input v-model="to" type="text" name="to" id="to" class="form-control form-control-sm" placeholder="End Date">
             </div>
         </div>
         <button class="btn btn-secondary btn-block">Check!</button>
     </div>
 </template>
+<script>
+export default {
+    data() {
+        return {
+            from: null,
+            to: null
+        }
+    }
+}
+</script>
 <style scoped>
 label {
     font-size: 0.7rem;
