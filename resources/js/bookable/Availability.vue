4d9d75e599ea9249fc3076d0438681bdd07b8b16
Sunasuna24 <sunasunayaka1218@gmail.com>
2022-09-24 22:47:21 +0900

    予約専用のコンポーネントを作成した

---
+++ resources/js/bookable/Availability.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <h6 class="text-uppercase text-secondary font-weight-bolder">Check Availability</h6>
+        <div class="form-row">
+            <div class="form-group col-md-6">
+                <label for="from">From</label>
+                <input type="text" name="from" id="from" class="form-control form-control-sm" placeholder="Start Date">
+            </div>
+            <div class="form-group col-md-6">
+                <label for="to">To</label>
+                <input type="text" name="to" id="to" class="form-control form-control-sm" placeholder="End Date">
+            </div>
+        </div>
+        <button class="btn btn-secondary btn-block">Check!</button>
+    </div>
+</template>
+<style scoped>
+label {
+    font-size: 0.7rem;
+    text-transform: uppercase;
+    color: gray;
+    font-weight: bolder;
+}
+</style>
