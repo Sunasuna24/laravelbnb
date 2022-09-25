4b362e6648c96a254e9fa468187d29c09ea5c15d
Sunasuna24 <sunasunayaka1218@gmail.com>
2022-09-25 15:27:55 +0900

    クリックしてレートが変化するのを確認した

--- public/js/app.js
+++ public/js/app.js
@@ -2099,6 +2099,33 @@ __webpack_require__.r(__webpack_exports__);
 
 /***/ }),
 
+/***/ "./node_modules/babel-loader/lib/index.js?!./node_modules/vue-loader/lib/index.js?!./resources/js/review/review.vue?vue&type=script&lang=js&":
+/*!*************************************************************************************************************************************************************!*\
+  !*** ./node_modules/babel-loader/lib??ref--4-0!./node_modules/vue-loader/lib??vue-loader-options!./resources/js/review/review.vue?vue&type=script&lang=js& ***!
+  \*************************************************************************************************************************************************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony default export */ __webpack_exports__["default"] = ({
+  data: function data() {
+    return {
+      review: {
+        rating: 5,
+        content: null
+      }
+    };
+  } // methods: {
+  //     onRatingChanged(rating) {
+  //         console.log(rating);
+  //     }
+  // }
+
+});
+
+/***/ }),
+
 /***/ "./node_modules/babel-loader/lib/index.js?!./node_modules/vue-loader/lib/index.js?!./resources/js/shared/components/StarRating.vue?vue&type=script&lang=js&":
 /*!****************************************************************************************************************************************************************************!*\
   !*** ./node_modules/babel-loader/lib??ref--4-0!./node_modules/vue-loader/lib??vue-loader-options!./resources/js/shared/components/StarRating.vue?vue&type=script&lang=js& ***!
@@ -2494,6 +2521,11 @@ var render = function render() {
     staticClass: "fa-3x",
     attrs: {
       rating: 5
+    },
+    on: {
+      "rating:changed": function ratingChanged($event) {
+        _vm.review.rating = $event;
+      }
     }
   })], 1), _vm._v(" "), _vm._m(0), _vm._v(" "), _c("button", {
     staticClass: "btn btn-large btn-primary btn-block"
@@ -2544,14 +2576,24 @@ var render = function render() {
   return _c("div", [_vm._l(_vm.fullStars, function (star) {
     return _c("i", {
       key: "full" + star,
-      staticClass: "fas fa-star"
+      staticClass: "fas fa-star",
+      on: {
+        click: function click($event) {
+          return _vm.$emit("rating:changed", star);
+        }
+      }
     });
   }), _vm._v(" "), _vm.halfStar ? _c("i", {
     staticClass: "fas fa-star-half-alt"
   }) : _vm._e(), _vm._v(" "), _vm._l(_vm.emptyStarts, function (star) {
     return _c("i", {
       key: "empty" + star,
-      staticClass: "far fa-star"
+      staticClass: "far fa-star",
+      on: {
+        click: function click($event) {
+          return _vm.$emit("rating:changed", _vm.fullStars + star);
+        }
+      }
     });
   })], 2);
 };
@@ -76176,15 +76218,17 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 "use strict";
 __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var _review_vue_vue_type_template_id_e002ee3e___WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./review.vue?vue&type=template&id=e002ee3e& */ "./resources/js/review/review.vue?vue&type=template&id=e002ee3e&");
-/* harmony import */ var _node_modules_vue_loader_lib_runtime_componentNormalizer_js__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ../../../node_modules/vue-loader/lib/runtime/componentNormalizer.js */ "./node_modules/vue-loader/lib/runtime/componentNormalizer.js");
+/* harmony import */ var _review_vue_vue_type_script_lang_js___WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./review.vue?vue&type=script&lang=js& */ "./resources/js/review/review.vue?vue&type=script&lang=js&");
+/* empty/unused harmony star reexport *//* harmony import */ var _node_modules_vue_loader_lib_runtime_componentNormalizer_js__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ../../../node_modules/vue-loader/lib/runtime/componentNormalizer.js */ "./node_modules/vue-loader/lib/runtime/componentNormalizer.js");
 
-var script = {}
+
+
 
 
 /* normalize component */
 
-var component = Object(_node_modules_vue_loader_lib_runtime_componentNormalizer_js__WEBPACK_IMPORTED_MODULE_1__["default"])(
-  script,
+var component = Object(_node_modules_vue_loader_lib_runtime_componentNormalizer_js__WEBPACK_IMPORTED_MODULE_2__["default"])(
+  _review_vue_vue_type_script_lang_js___WEBPACK_IMPORTED_MODULE_1__["default"],
   _review_vue_vue_type_template_id_e002ee3e___WEBPACK_IMPORTED_MODULE_0__["render"],
   _review_vue_vue_type_template_id_e002ee3e___WEBPACK_IMPORTED_MODULE_0__["staticRenderFns"],
   false,
@@ -76198,6 +76242,20 @@ var component = Object(_node_modules_vue_loader_lib_runtime_componentNormalizer_
 if (false) { var api; }
 component.options.__file = "resources/js/review/review.vue"
 /* harmony default export */ __webpack_exports__["default"] = (component.exports);
+
+/***/ }),
+
+/***/ "./resources/js/review/review.vue?vue&type=script&lang=js&":
+/*!*****************************************************************!*\
+  !*** ./resources/js/review/review.vue?vue&type=script&lang=js& ***!
+  \*****************************************************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _node_modules_babel_loader_lib_index_js_ref_4_0_node_modules_vue_loader_lib_index_js_vue_loader_options_review_vue_vue_type_script_lang_js___WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! -!../../../node_modules/babel-loader/lib??ref--4-0!../../../node_modules/vue-loader/lib??vue-loader-options!./review.vue?vue&type=script&lang=js& */ "./node_modules/babel-loader/lib/index.js?!./node_modules/vue-loader/lib/index.js?!./resources/js/review/review.vue?vue&type=script&lang=js&");
+/* empty/unused harmony star reexport */ /* harmony default export */ __webpack_exports__["default"] = (_node_modules_babel_loader_lib_index_js_ref_4_0_node_modules_vue_loader_lib_index_js_vue_loader_options_review_vue_vue_type_script_lang_js___WEBPACK_IMPORTED_MODULE_0__["default"]); 
 
 /***/ }),
 
--- resources/js/review/Review.vue
+++ resources/js/review/Review.vue
@@ -2,7 +2,11 @@
     <div>
         <div class="form-group">
             <label for="" class="text-muted">Select the Star Rating (1 is worst, 5 is best)</label>
-            <star-rating :rating="5" class="fa-3x"></star-rating>
+            <star-rating
+                :rating="5"
+                class="fa-3x"
+                v-on:rating:changed="review.rating = $event"
+            ></star-rating>
         </div>
         <div class="form-group">
             <label for="content" class="text-muted">Descript your Experience with</label>
@@ -11,3 +15,20 @@
         <button class="btn btn-large btn-primary btn-block">Submit</button>
     </div>
 </template>
+<script>
+export default {
+    data() {
+        return {
+            review: {
+                rating: 5,
+                content: null
+            }
+        }
+    }
+    // methods: {
+    //     onRatingChanged(rating) {
+    //         console.log(rating);
+    //     }
+    // }
+}
+</script>
