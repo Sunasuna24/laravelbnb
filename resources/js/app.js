db81898cf09233611dfd2869cb1e426eb31a81d6
Sunasuna24 <sunasunayaka1218@gmail.com>
2022-09-26 23:06:06 +0900

    FatalError用のコンポーネントを作成した

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,18 +5,21 @@ import router from "./routes";
 import Index from "./Index";
 import moment from 'moment';
 import StarRating from "./shared/components/StarRating";
+import FatalError from "./shared/components/FatalError";
 
 window.Vue = require('vue');
 
 Vue.use(VueRouter);
 
 Vue.filter("fromNow", value => moment(value).fromNow());
+
 Vue.component("star-rating", StarRating);
+Vue.component("fatal-error", FatalError);
 
 const app = new Vue({
     el: '#app',
     router: router,
     components: {
-        "index": Index
+        index: Index
     }
 });
